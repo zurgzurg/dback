f457a8f5784c16c9a1f3764657fc86d718ab896e
rambham <rambham@dead9614-3a6c-c913-387a-c05425e1b5c3>
2011-10-12 00:10:05 +0000

    getting started

--- trunk/include/dback.h
+++ trunk/include/dback.h
@@ -3,10 +3,45 @@
 
 namespace dback {
 
-    class Message {
+    class MessageContext;
+
+    /**
+     * Class for various errors.
+     *
+     */
+    class Error {
+    };
+
+
+    /**
+     * Base class to support serialization.
+     *
+     * Provides some basic routines that are
+     */
+    class SerialBase {
+	/**
+	 * The 
+	 *
+	 */
+	virtual void toBytes(SerialBuffer *, MessageContext *) = 0;
+
+	virtual Message *fromBytes(SerialBuffer *, MessageContext *) = 0;
+    };
+
+    /**
+     * Instances of this class are used to serialize and deserialize messages.
+     * 
+     */
+    class SerialBuffer {
 	
     };
 
+    /**
+     * Contains higher level state used to serialize and deserialize messages.
+     */
+    class MessageContext {
+
+    };
 }
 
 #endif
