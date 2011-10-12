cbef1398071a1ba60d06c97d1f02e32ecee1e6ea
rambham@gmail.com <rambham@gmail.com@dead9614-3a6c-c913-387a-c05425e1b5c3>
2011-10-12 15:03:31 +0000

    need the C++ namespace feature

--- trunk/include/dback.h
+++ trunk/include/dback.h
@@ -3,7 +3,11 @@
 
 namespace dback {
 
+    /// all serialization memory buffers are arrays this type
+    typedef unsigned char uchar;
+
     class MessageContext;
+    class Message;
 
     /**
      * Class for various errors.
@@ -16,16 +20,40 @@ namespace dback {
     /**
      * Base class to support serialization.
      *
-     * Provides some basic routines that are
+     * All objects that are intended to be serialized should derive
+     * from this class.
      */
     class SerialBase {
+    public:
 	/**
-	 * The 
+	 * Used to generate bytes into a buffer.
 	 *
+	 * @param[out] sb Bytes are written into this buffer.
+	 * @param[in]  mc Context contains data that the message might need.
+	 * @param[in]  idx The index into the SerialBuffer where output should
+	 *                 be written.
 	 */
-	virtual void toBytes(SerialBuffer *, MessageContext *) = 0;
+	virtual void toBytes(SerialBuffer *sb,
+			     MessageContext *mc,
+			     size_t idx) = 0;
 
-	virtual Message *fromBytes(SerialBuffer *, MessageContext *) = 0;
+	/**
+	 * Used to transform a series of bytes into a message.
+	 *
+	 * @param[in] sb Bytes are read from this buffer.
+	 * @param[in] mc Context contains memory allocation funcs and data.
+	 * @param[in] idx Where reading should start.
+	 *
+	 * @exception Error Thrown if there is an error in decoding the bytes.
+	 *  for example if the bytes do not represent a valid message.
+	 *
+	 * @return a Message object pointer. The object and any other memory
+	 * will be allocated using from the MessageContext object. Caller
+	 * is responsible for freeing all memory.
+	 */
+	virtual Message *fromBytes(SerialBuffer *sb,
+				   MessageContext *mc,
+				   size_t idx) = 0;
     };
 
     /**
@@ -33,15 +61,40 @@ namespace dback {
      * 
      */
     class SerialBuffer {
-	
+    private:
+	/// the buffer
+	uchar *buf;
+
+	/// size of the buffer
+	size_t sizeOfBuf;
+
+	/// the index of where to write the next byte
+	size_t writeIdx;
+
+	/// the index of the next byte to read
+	size_t readIdx;
+
+    public:
+	/**
+	 * @param [in] bf The serialization buffer to use.
+	 * @param [in] sz Size of bf.
+	 */
+	SerialBuffer(uchar *bf, size_t sz);
     };
 
     /**
      * Contains higher level state used to serialize and deserialize messages.
      */
     class MessageContext {
-
+      
     };
 }
+
+/*
+Local Variables:
+mode: c++
+c-basic-offset: 4
+End:
+*/
 
 #endif
