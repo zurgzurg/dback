ed6d79f6b202e79d40197e9cc7a0b14c2bb1727a
rambham <rambham@gmail.com>
2011-10-16 05:35:55 +0000

    make the coverage directory be a generated artifact

--- autoclean.sh
+++ autoclean.sh
@@ -12,12 +12,14 @@ rm -f m4/ltsugar.m4 m4/libtool.m4 m4/ltversion.m4 m4/lt~obsolete.m4
 rm -f m4/ltoptions.m4
 rm -f include/Makefile.in
 
-rm -f src/*.o src/*.lo src/Makefile.in src/*~ src/utests src/*.a src/*.la
-rm -f src/stamp-h1 src/config.h src/Makefile src/config.h.in
 rm -f include/Makefile include/Makefile.in
 
+rm -f src/*.o src/*.lo src/Makefile.in src/*~ src/utests src/*.a src/*.la
+rm -f src/config.h src/Makefile src/config.h.in src/*.gcov
+rm -f src/stamp-h1 src/*.gcno
+rm -fr src/.libs src/.deps src/coverage src/coverage-stamp
 
-rm -fr autom4te.cache .deps src/.libs src/.deps
+rm -fr autom4te.cache .deps 
 
 rm -f doxyfile texput.log
 rm -fr doxygen-doc
