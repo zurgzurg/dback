3b584aa874a2163c1b43aaafda135f424a40254e
rambham <rambham@gmail.com>
2011-10-24 01:48:43 +0000

    first simple btree test case passing

--- src/btree.cpp
+++ src/btree.cpp
@@ -1,10 +1,37 @@
 #include <inttypes.h>
 #include <cstddef>
+#include <string>
 
 #include <arpa/inet.h>
 
 #include "dback.h"
 #include "btree.h"
 
-static int foo = 0;
+namespace dback {
+
+bool
+BTree::insertInLeaf(PageAccess *ac, KeyInterface *ki, ErrorInfo *err)
+{
+    if (ac->header->isLeaf != 1)
+	return false;
+
+    if (ac->header->numKeys + 1 >= this->header->maxNumLeafKeys)
+	return false;
+    
+    return true;
+}
+
+/****************************************************/
+/****************************************************/
+/* UUID key support                                 */
+/****************************************************/
+/****************************************************/
+int
+UUIDKey::compare(const uint8_t *a, const uint8_t *b)
+{
+    return 0;
+}
+
+}
+
 
